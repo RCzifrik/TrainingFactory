4d8e164ba8ad9df17d515c140fb55cd155b7a8d1
RCzifrik <94119911+RCzifrik@users.noreply.github.com>
2023-06-13 10:18:45 +0200

    guest pages created
    guest pages can be loaded, osme pages heavily require database functionality so those have a "In development" note. Nextstep is to build the Database

--- src/Controller/GuestController.php
+++ src/Controller/GuestController.php
@@ -11,8 +11,48 @@ class GuestController extends AbstractController
     #[Route('/', name: 'guest_home')]
     public function home(): Response
     {
-        return $this->render('guest/index.html.twig', [
-            'controller_name' => 'GuestController',
-        ]);
+        return $this->render('guest/index.html.twig');
+    }
+
+    #[Route('/login', name: 'guest_login')]
+    public function login(): Response
+    {
+        return $this->render('guest/login.html.twig');
+    }
+
+    #[Route('/register', name: 'guest_register')]
+    public function register(): Response
+    {
+        return $this->render('guest/register.html.twig');
+    }
+
+    #[Route('/contact', name: 'guest_contact')]
+    public function contact(): Response
+    {
+        return $this->render('guest/contact.html.twig');
+    }
+
+    #[Route('/trainerOverzicht', name: 'guest_trainerOverzicht')]
+    public function trainerOverzicht(): Response
+    {
+        return $this->render('guest/trainerOverzicht.html.twig');
+    }
+
+    #[Route('/trainerDetail', name: 'guest_trainerDetail')]
+    public function trainerDetail(): Response
+    {
+        return $this->render('guest/trainerDetail.html.twig');
+    }
+
+    #[Route('/trainingOverzicht', name: 'guest_trainingOverzicht')]
+    public function trainingOverzicht(): Response
+    {
+        return $this->render('guest/trainingOverzicht.html.twig');
+    }
+
+    #[Route('/trainingDetail', name: 'guest_trainingDetail')]
+    public function trainingDetail(): Response
+    {
+        return $this->render('guest/trainingDetail.html.twig');
     }
 }
